fd7c47671542b00785f943d286a7a3389d597eb8
Jarvas <daniel.faria@outlook.pt>
2022-01-04 21:58:56 +0000

    Iterativa inacabada

--- Fase_2/src/iterativa.pl
+++ Fase_2/src/iterativa.pl
@@ -2,3 +2,21 @@
 % gerar circuitos
 
 % Algoritmo
+%iterativa(Orig,Dest,Cam/C,0).
+iterativa(Orig,Dest,Cam/C,Iteracoes):-
+    dfs_iterativa(Orig,Dest,Cam/C,Iteracoes).
+
+dfs_iterativa(Orig,Dest,Cam/C,Iteracoes):- 
+    dfs2_iterativa(Orig,Dest,[Orig]/0,Cam/C,Iteracoes).
+
+dfs2_iterativa(Dest,Dest,LA/Custo,Cam/Custo,Iteracoes):- 
+    reverse(LA,Cam).
+dfs2_iterativa(Act,Dest,LA/Custo_LA,Cam/C,Iteracoes):- 
+    adjacente(Act,X,Custo_aresta), 
+    \+ member(X,LA), 
+    Custo is Custo_aresta+Custo_LA,
+    Iteracoes > 0,
+    Iteracoes_recursividade is Iteracoes-1,
+    dfs2_iterativa(X,Dest,[X|LA]/Custo,Cam/C,Iteracoes_recursividade).
+ 
+    
