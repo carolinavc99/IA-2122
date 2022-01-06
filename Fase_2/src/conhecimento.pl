ae03238e23140c04b4c3bdb6ed473f80451c2954
Jarvas <daniel.faria@outlook.pt>
2022-01-06 16:46:27 +0000

    Invariantes inserção

--- Fase_2/src/conhecimento.pl
+++ Fase_2/src/conhecimento.pl
@@ -1,4 +1,13 @@
 % ------ CONHECIMENTO ------
+:- op(900,xfy,'::').
+:- dynamic veiculo/5.
+:- dynamic estafeta/2.
+:- dynamic freguesia/1.
+:- dynamic rua/2.
+:- dynamic cliente/3.
+:- dynamic entrega/5.
+:- dynamic encomenda/8.
+
 % --------------------------------------
 % PMF
 % --------------------------------------
@@ -29,3 +38,84 @@
 -entrega(Identificacao,Encomenda,Estafeta,Classificacao,Veiculo):-
     not(entrega(Identificacao,Encomenda,Estafeta,Classificacao,Veiculo)),
     not(excecao(entrega(Identificacao,Encomenda,Estafeta,Classificacao,Veiculo))).
+
+% ------------------------------------------------------------------------------------------------
+% Invariantes de Repetição
+% ------------------------------------------------------------------------------------------------
+
++veiculo(Tipo,Carga,Velocidade,Preco,Decrescimento)::
+    (
+        findall(
+            (Tipo,Carga,Velocidade,Preco,Decrescimento),
+            veiculo(Tipo,Carga,Velocidade,Preco,Decrescimento),
+            S
+        ),
+        length(S,N),
+        N==1
+    ).
+
++estafeta(Identificacao,Nome)::
+    (
+        findall(
+            ((Identificacao,Nome),
+            estafeta(Identificacao,Nome),
+            S)
+        ),
+        length(S,N),
+        N==1
+    ).
+
++encomenda(Identificacao,Datahora,Tempo_Entrega,Peso,Volume,Preco,Rua,Cliente)::
+    (
+        findall(
+            ((Identificacao,Datahora,Tempo_Entrega,Peso,Volume,Preco,Rua,Cliente),
+            encomenda(Identificacao,Datahora,Tempo_Entrega,Peso,Volume,Preco,Rua,Cliente),
+            S)
+        ),
+        length(S,N),
+        N==1
+    ).
+
++freguesia(Identificacao)::
+    (
+        findall(
+            ((Identificacao),
+            freguesia(Identificacao),
+            S)
+        ),
+        length(S,N),
+        N==1
+    ).
+
++rua(Identificacao,Freguesia)::
+    (
+        findall(
+            ((Identificacao,Freguesia),
+            rua(Identificacao,Freguesia),
+            S)
+        ),
+        length(S,N),
+        N==1
+    ).
+
++cliente(Identificacao,Nome,Rua)::
+    (
+        findall(
+            ((Identificacao,Nome,Rua),
+            cliente(Identificacao,Nome,Rua),
+            S)
+        ),
+        length(S,N),
+        N==1
+    ).
+
++entrega(Identificacao,Encomenda,Estafeta,Classificacao,Veiculo)::
+    (
+        findall(
+            ((Identificacao,Encomenda,Estafeta,Classificacao,Veiculo),
+            entrega(Identificacao,Encomenda,Estafeta,Classificacao,Veiculo),
+            S)
+        ),
+        length(S,N),
+        N==1
+    ).
