bb1a9920c7cd99c2d1c2bc58d1ce8d1121737671
carolinavc99 <48517052+carolinavc99@users.noreply.github.com>
2022-01-07 23:15:22 +0000

    tempo done

--- Fase_2/src/teste_tempo.pl
+++
@@ -1,8 +0,0 @@
-% Testar o tempo de cada função
-
-% datahora modificada para debitar segundos e milissegundos
-% chamar função antes e depois de chamar a função a testar
-% comparar horas
-
-
-% maybe call_time/2?
