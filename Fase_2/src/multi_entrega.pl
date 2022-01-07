076cd2630c3ebf7803c0c194d82a8b5c02a76ae1
Jarvas <daniel.faria@outlook.pt>
2022-01-07 19:56:55 +0000

    caminhos par amulti entrega

--- Fase_2/src/multi_entrega.pl
+++ Fase_2/src/multi_entrega.pl
@@ -5,18 +5,46 @@
 
 %multi_circuito(gulosa,Ruas,Caminho,Custo,Peso,Volume).
 %tempo_de_entrega(VelocidadeBaseVeiculo, DecrescimoVelocidadeVeiculo, Peso, Distancia, TempoViagem)
-multi_circuito(profundidade,Ruas,Caminho,Custo,Peso):-
+multi_circuito(profundidade,Ruas,Caminho,TempoViagem,Peso):-
     get_caminho_profundidade(Ruas,Caminho,Custo),
-    tempo_de_entrega(45)
+    tempo_de_entrega(45,0,Peso,Custo,TempoViagem).
     %Caminho de centro para rua1
     %rua1 para rua2 etc
     %juntar caminhos em Caminho
-multi_circuito(largura,Ruas,Caminho,Custo,Peso).
+multi_circuito(largura,Ruas,Caminho,Custo,Peso):-
+    get_caminho_largura(Ruas,Caminho,Custo).
 
-multi_circuito(iterativa,Ruas,Caminho,Custo,Peso).
+multi_circuito(iterativa,Ruas,Caminho,Custo,Peso):-
+    get_caminho_iterativa(Ruas,Caminho,Custo).
 
+get_caminho_iterativa([Local],Cam,C):-
+    iterativa(Local,centro,Cam/C).
+get_caminho_iterativa([Local_1,Local_2|T],Caminho,Custo):-
+    iterativa(Local_1,Local_2,Cam/C),
+    append(Cam,Tail,Caminho),
+    get_caminho_iterativa([Local_2|T],[H|Tail],Custo_Recursividade),
+    Custo is C+Custo_Recursividade.
+get_caminho_iterativa([Local_1,Local_2|T],Caminho,Custo):-
+    iterativa(Local_1,Local_2,Cam/C),
+    append(Cam,Caminho_Recursividade,Caminho),
+    get_caminho_iterativa([Local_2|T],Caminho_Recursividade,Custo_Recursividade),
+    Custo is C+Custo_Recursividade.
 
-get_caminho_profundidade([Local],[],0).
+get_caminho_largura([Local],Cam,C):-
+    bfs(Local,centro,Cam/C).
+get_caminho_largura([Local_1,Local_2|T],Caminho,Custo):-
+    bfs(Local_1,Local_2,Cam/C),
+    append(Cam,Tail,Caminho),
+    get_caminho_largura([Local_2|T],[H|Tail],Custo_Recursividade),
+    Custo is C+Custo_Recursividade.
+get_caminho_largura([Local_1,Local_2|T],Caminho,Custo):-
+    bfs(Local_1,Local_2,Cam/C),
+    append(Cam,Caminho_Recursividade,Caminho),
+    get_caminho_largura([Local_2|T],Caminho_Recursividade,Custo_Recursividade),
+    Custo is C+Custo_Recursividade.
+
+get_caminho_profundidade([Local],Cam,C):-
+    dfs(Local,centro,Cam/C).
 get_caminho_profundidade([Local_1,Local_2|T],Caminho,Custo):-
     dfs(Local_1,Local_2,Cam/C),
     append(Cam,Tail,Caminho),
